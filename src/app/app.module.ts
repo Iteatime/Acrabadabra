776db11483c2df901f70a269f65c817c3c497ab0
MrClemds <clement.pont@mrclemds.fr>
2019-01-11 19:08:38 +0100

    refactor(timesheetService): Increase the code quality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,34 @@ import { HomeComponent } from './components/home/home.component';
 
 import { TimesheetModule } from './modules/timesheet/timesheet.module';
 
-registerLocaleData(localeFr, 'fr');
+import { EditTimesheetComponent } from './edit-timesheet/edit-timesheet.component';
+import { InvoiceFormComponent } from './edit-timesheet/invoice-form/invoice-form.component';
+import { ReviewTimesheetComponent } from './review-timesheet/review-timesheet.component';
+
+import { CalendarComponent } from './calendar/calendar.component';
+
+import { CopyToClipboardDirective } from './shared/copy-to-clipboard.directive';
+import { MailtoDirective } from './shared/mailto.directive';
+
+import { PdfInvoiceComponent } from './pdf-invoice/pdf-invoice.component';
+
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+
+    EditTimesheetComponent,
+    InvoiceFormComponent,
+    ReviewTimesheetComponent,
+
+    CalendarComponent,
+
+    CopyToClipboardDirective,
+    MailtoDirective,
+
+    PdfInvoiceComponent,
   ],
   imports: [
     AppRoutingModule,
--- src/app/modules/calendar/calendar.service.spec.ts
+++ src/app/modules/calendar/calendar.service.spec.ts
@@ -1,12 +1,46 @@
-import { TestBed } from '@angular/core/testing';
+import { TimesheetService } from './timesheet.service';
+import { Timesheet } from './timesheet.model';
 
-import { CalendarService } from './calendar.service';
+describe('TimesheetService -', () => {
 
-describe('CalendarService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: TimesheetService;
+  const timesheet = new Timesheet();
+  const getEditToken = () => btoa(unescape(encodeURIComponent(JSON.stringify({ mode: 'edit', timesheet }))));
+  const getReviewToken = () => btoa(unescape(encodeURIComponent(JSON.stringify({ mode: 'review', timesheet }))));
+
+  beforeEach(() => {
+    service = new TimesheetService();
+    service.openTimesheet(getReviewToken(), 'review');
+  });
 
   it('should be created', () => {
-    const service: CalendarService = TestBed.get(CalendarService);
     expect(service).toBeTruthy();
+  });
+
+  describe('openTimesheet())', () => {
+    it('should return false if the "mode" argument does not match the "mode" stored in the token', () => {
+      expect(service.openTimesheet(getReviewToken(), 'edit')).toBeFalsy();
+    });
+    describe('if the "mode" argument match the "mode" stored in the token', () => {
+      it('should return true', () => {
+        service.openTimesheet(getEditToken(), 'edit');
+      });
+      it('should set the timesheet properties from those stored in the token', () => {
+        service.openTimesheet(getEditToken(), 'edit');
+        expect(JSON.stringify(timesheet) === JSON.stringify(service.timesheet)).toBeTruthy();
+      });
+    });
+  });
+
+  describe('getEditToken())', () => {
+    it('should return a base64 encoded json object containing the timesheet property, and a "mode" property set to "edit"', () => {
+      expect(service.getEditToken()).toBe(getEditToken());
+    });
+  });
+
+  describe('getReviewToken())', () => {
+    it('should return a base64 encoded json object containing the timesheet property, and a "mode" property set to "review"', () => {
+      expect(service.getReviewToken()).toBe(getReviewToken());
+    });
   });
 });
