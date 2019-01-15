1e0f0e17b360788a9e274e1573d10194dbbfdf3f
MrClemds <clement.pont@mrclemds.fr>
2019-01-15 10:00:39 +0100

    test(TimesheetToken): Add a new test case

--- src/app/modules/calendar/calendar.service.spec.ts
+++ src/app/modules/calendar/calendar.service.spec.ts
@@ -4,13 +4,12 @@ import { Timesheet } from './timesheet.model';
 describe('TimesheetService -', () => {
 
   let service: TimesheetService;
-  const timesheet = new Timesheet();
-  const getEditToken = () => btoa(unescape(encodeURIComponent(JSON.stringify({ mode: 'edit', timesheet }))));
-  const getReviewToken = () => btoa(unescape(encodeURIComponent(JSON.stringify({ mode: 'review', timesheet }))));
+  const testTimesheet = new Timesheet('test');
+  const getEditToken = btoa(unescape(encodeURIComponent(JSON.stringify({ mode: 'edit', timesheet: testTimesheet }))));
+  const getReviewToken =  btoa(unescape(encodeURIComponent(JSON.stringify({ mode: 'review', timesheet: testTimesheet }))));
 
   beforeEach(() => {
     service = new TimesheetService();
-    service.openTimesheet(getReviewToken(), 'review');
   });
 
   it('should be created', () => {
@@ -19,28 +18,41 @@ describe('TimesheetService -', () => {
 
   describe('openTimesheet())', () => {
     it('should return false if the "mode" argument does not match the "mode" stored in the token', () => {
-      expect(service.openTimesheet(getReviewToken(), 'edit')).toBeFalsy();
+      expect(service.openTimesheet(getReviewToken, 'edit')).toBeFalsy();
     });
+
     describe('if the "mode" argument match the "mode" stored in the token', () => {
       it('should return true', () => {
-        service.openTimesheet(getEditToken(), 'edit');
+        expect(service.openTimesheet(getEditToken, 'edit')).toBeTruthy();
       });
-      it('should set the timesheet properties from those stored in the token', () => {
-        service.openTimesheet(getEditToken(), 'edit');
-        expect(JSON.stringify(timesheet) === JSON.stringify(service.timesheet)).toBeTruthy();
+
+      it('should create an instance of Timesheet', () => {
+        service.openTimesheet(getEditToken, 'edit');
+        expect(service.timesheet instanceof Timesheet).toBeTruthy();
+      });
+
+      it('should copy the data to the new timesheet', () => {
+        service.openTimesheet(getEditToken, 'edit');
+        expect(service.timesheet.consultant.email).toBe('test');
       });
     });
   });
 
-  describe('getEditToken())', () => {
-    it('should return a base64 encoded json object containing the timesheet property, and a "mode" property set to "edit"', () => {
-      expect(service.getEditToken()).toBe(getEditToken());
+  describe('when oppening a token', () => {
+    beforeEach(() => {
+      service.timesheet = this.timesheet;
     });
-  });
 
-  describe('getReviewToken())', () => {
-    it('should return a base64 encoded json object containing the timesheet property, and a "mode" property set to "review"', () => {
-      expect(service.getReviewToken()).toBe(getReviewToken());
+    describe('getEditToken())', () => {
+      it('should return a base64 encoded json object containing the timesheet property, and a "mode" property set to "edit"', () => {
+        expect(service.getEditToken()).toBe(getEditToken);
+      });
+    });
+
+    describe('getReviewToken())', () => {
+      it('should return a base64 encoded json object containing the timesheet property, and a "mode" property set to "review"', () => {
+        expect(service.getReviewToken()).toBe(getReviewToken);
+      });
     });
   });
 });
