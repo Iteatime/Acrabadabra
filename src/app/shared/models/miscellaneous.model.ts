110d5012087ff0442311070ef5fe065f6d9c6762
Mathieu Kanel <mathieu.kanel@gmail.com>
2019-03-19 09:51:59 +0100

    feat(app): hotfix on invoice

--- src/app/shared/models/miscellaneous.model.ts
+++ src/app/shared/models/miscellaneous.model.ts
@@ -2,7 +2,7 @@ export class Miscellaneous {
 
     constructor(
         public miscellaneousType?: string,
-        public tvaRate?: number,
+        public tvaRate?: any,
         public wording?: string,
         public date?: string,
         public amount?: number,
