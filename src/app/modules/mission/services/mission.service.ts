bd67b4546e6f824b95dfffcbd6efda5b137093f7
Mathieu Kanel <mathieu.kanel@gmail.com>
2019-05-28 14:07:24 +0200

    fixup! First try on feature

---
+++ src/app/modules/mission/services/mission.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Mission } from 'src/app/shared/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MissionService {
+
+  public mission = new Mission();
+
+
+  constructor() { }
+
+  createTodo(data) {
+    console.log(data);
+    return fetch('/.netlify/functions/todos-create', {
+      body: JSON.stringify(data),
+      method: 'POST'
+    }).then(response => {
+      return response.json();
+    });
+  }
+
+}
+
