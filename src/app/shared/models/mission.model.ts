bf16b03a81d3ab46019d91b6fe36628511dfa73c
Mathieu Kanel <mathieu.kanel@gmail.com>
2019-05-28 11:38:10 +0200

    First try on feature

--- src/app/shared/models/mission.model.ts
+++ src/app/shared/models/mission.model.ts
@@ -1,9 +1,36 @@
 export class Mission {
+  consultant: string;
+  consultantEmail: string;
   client: string;
+  clientEmail: string;
   title: string;
+  clientRef: string;
+  startDate: string;
+  endDate: string;
+  unitOfWorkType: string;
+  unitOfworkPrice: string;
 
-  constructor(missionClient: string, missionTitle: string) {
+  constructor(
+    missionClient?: string,
+    missionTitle?: string,
+    missionConsultant?: string,
+    missionConsultantEmail?: string,
+    missionClientEmail?: string,
+    missionclientRef?: string,
+    missionStartDate?: string,
+    missionEndDate?: string,
+    missionUnitOfWorkType?: string,
+    missionUnitOfWorkPrice?: string) {
+
+    this.consultant = missionConsultant;
+    this.consultantEmail = missionConsultantEmail;
     this.client = missionClient;
+    this.clientEmail = missionClientEmail;
     this.title = missionTitle;
+    this.clientRef = missionclientRef;
+    this.startDate = missionStartDate;
+    this.endDate = missionEndDate;
+    this.unitOfWorkType = missionUnitOfWorkType;
+    this.unitOfworkPrice = missionUnitOfWorkPrice;
   }
 }
