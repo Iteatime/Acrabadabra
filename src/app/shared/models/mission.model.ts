0d96e786b5c2ab887213ce347345ed77e742b586
Mathieu Kanel <mathieu.kanel@gmail.com>
2019-06-04 10:19:19 +0200

    fixup! fix main bug

--- src/app/shared/models/mission.model.ts
+++ src/app/shared/models/mission.model.ts
@@ -1,8 +1,26 @@
 export class Mission {
+  missionCreator: string;
+  consultant: string;
+  consultantEmail: string;
   client: string;
   title: string;
 
-  constructor(missionClient: string, missionTitle: string) {
+  constructor(
+    missionCreator?: string,
+    missionClient?: string,
+    missionTitle?: string,
+    missionConsultant?: string,
+    missionConsultantEmail?: string,
+    missionClientEmail?: string,
+    missionclientRef?: string,
+    missionStartDate?: string,
+    missionEndDate?: string,
+    missionUnitOfWorkType?: string,
+    missionUnitOfWorkPrice?: string) {
+
+    this.missionCreator = missionCreator;
+    this.consultant = missionConsultant;
+    this.consultantEmail = missionConsultantEmail;
     this.client = missionClient;
     this.title = missionTitle;
   }
