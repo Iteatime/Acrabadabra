e83cd9a2445d6d87fd4b89347bef7c07b1ae59c6
Mathieu Kanel <mathieu.kanel@gmail.com>
2019-06-13 10:31:25 +0200

    add transfert mission data in timesheet using service

--- src/app/modules/mission/mission.routes.ts
+++ src/app/modules/mission/mission.routes.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from '../dashboard/components/dashboard/dashboard.component';
+import { TimesheetEditComponent } from '../timesheet/components/timesheet-edit/timesheet-edit.component';
 
 const routes: Routes = [
 
   { path: 'dashboard', component: DashboardComponent },
+
 
 ];
 
