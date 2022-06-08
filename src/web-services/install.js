26606e3afaf7d5742e55651f9a13ff15135ebf62
Julien Lavocat <julien.lavocat@gmail.com>
2022-06-08 14:24:14 +0200

    refactor(pdf) remove git submodule

--- src/web-services/install.js
+++ src/web-services/install.js
@@ -1,22 +1,29 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const concurrently = require('concurrently');
+const concurrently = require("concurrently");
 
 const folder = path.resolve(__dirname);
 const services = [];
 
 fs.readdir(folder, (err, elements) => {
-  elements.forEach(element => {
+  elements.forEach((element) => {
     const dir = path.resolve(folder, element);
-    if (fs.lstatSync(dir).isDirectory() && fs.existsSync(dir + '/package.json')) {
-      services.push({ command: `cd ${ dir } && npm install`, name: element, prefixColor: 'reset.bgGreen.bold' });
+    if (
+      fs.lstatSync(dir).isDirectory() &&
+      fs.existsSync(dir + "/package.json")
+    ) {
+      services.push({
+        command: `cd ${dir} && yarn`,
+        name: element,
+        prefixColor: "reset.bgGreen.bold",
+      });
     }
   });
 
   if (services.length > 0) {
     concurrently(services, {
-      prefix: 'name'
+      prefix: "name",
     });
   }
 });
