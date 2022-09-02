953b512e2f42d3eda339be1270953b3d0856e8d0
Julien Lavocat <julien.lavocat@gmail.com>
2022-09-02 10:42:05 +0200

    fix: update components to v13 and fix css

--- src/app/modules/expense/components/expense-mileage-form/expense-mileage-form.component.ts
+++ src/app/modules/expense/components/expense-mileage-form/expense-mileage-form.component.ts
@@ -1,34 +1,44 @@
-import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  Input,
+  Output,
+  EventEmitter,
+  AfterViewInit,
+} from "@angular/core";
+import { NgForm } from "@angular/forms";
 
-import { Commute } from '../../../../shared/models/commute';
+import { Commute } from "../../../../shared/models/commute";
 
-import { TimesheetService } from 'src/app/modules/timesheet/services/timesheet.service';
-import { VehiclesService } from '../../services/vehicles.service';
-
+import { TimesheetService } from "src/app/modules/timesheet/services/timesheet.service";
+import { VehiclesService } from "../../services/vehicles.service";
 
 @Component({
-  selector: 'app-expense-mileage-form',
-  templateUrl: './expense-mileage-form.component.html',
-  styleUrls: ['./expense-mileage-form.component.scss']
+  selector: "app-expense-mileage-form",
+  templateUrl: "./expense-mileage-form.component.html",
+  styleUrls: ["./expense-mileage-form.component.scss"],
 })
-export class ExpenseMileageFormComponent implements OnInit {
-
-  @ViewChild('expenseForm') form: NgForm;
+export class ExpenseMileageFormComponent implements OnInit, AfterViewInit {
+  @ViewChild("expenseForm") form: NgForm;
   @Input() commutes: Commute[];
   @Output() changed: EventEmitter<boolean> = new EventEmitter();
 
-  commute = new Commute('', '', null, '', null);
+  commute = new Commute("", "", null, "", null);
   submitted = false;
   vehicles: any[];
 
-  constructor(public vehiclesService: VehiclesService,
-              public timesheetService: TimesheetService) { }
+  constructor(
+    public vehiclesService: VehiclesService,
+    public timesheetService: TimesheetService
+  ) {}
 
   ngOnInit() {
     this.vehicles = this.vehiclesService.vehicles;
     this.commutes = this.timesheetService.timesheet.commutes;
+  }
 
+  ngAfterViewInit(): void {
     this.form.valueChanges.subscribe(() => {
       if (this.form.dirty) {
         this.changed.emit(true);
@@ -39,14 +49,16 @@ export class ExpenseMileageFormComponent implements OnInit {
   onSubmit() {
     if (this.form.valid) {
       if (!this.vehiclesService.isCustomizable(this.commute)) {
-        this.commute.allowance = this.vehiclesService.vehicles[this.commute.vehicleSelected].allowance;
+        this.commute.allowance =
+          this.vehiclesService.vehicles[this.commute.vehicleSelected].allowance;
       }
       this.submitted = true;
-      this.commute.mileageAllowance = this.commute.distance * this.commute.allowance;
-      this.commutes.push({...this.commute});
+      this.commute.mileageAllowance =
+        this.commute.distance * this.commute.allowance;
+      this.commutes.push({ ...this.commute });
       this.changed.emit(true);
     } else {
-      Object.keys(this.form.controls).forEach(field => {
+      Object.keys(this.form.controls).forEach((field) => {
         this.form.controls[field].markAsTouched();
       });
     }
--- src/app/modules/expense/components/expense-miscellaneous-form/expense-miscellaneous-form.component.ts
+++ src/app/modules/expense/components/expense-miscellaneous-form/expense-miscellaneous-form.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit, ViewChild, Input, EventEmitter, Output } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Miscellaneous } from 'src/app/shared/models/miscellaneous.model';
-import { MonetaryService } from 'src/app/shared/services/monetary/monetary.service';
-import { TimesheetService } from 'src/app/modules/timesheet/services/timesheet.service';
-import { MiscellaneousExpensesService } from '../../services/miscellaneous-expenses.service';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  Input,
+  EventEmitter,
+  Output,
+  AfterViewInit,
+} from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Miscellaneous } from "src/app/shared/models/miscellaneous.model";
+import { MonetaryService } from "src/app/shared/services/monetary/monetary.service";
+import { TimesheetService } from "src/app/modules/timesheet/services/timesheet.service";
+import { MiscellaneousExpensesService } from "../../services/miscellaneous-expenses.service";
 
 @Component({
-  selector: 'app-expense-miscellaneous-form',
-  templateUrl: './expense-miscellaneous-form.component.html',
-  styleUrls: ['./expense-miscellaneous-form.component.scss']
+  selector: "app-expense-miscellaneous-form",
+  templateUrl: "./expense-miscellaneous-form.component.html",
+  styleUrls: ["./expense-miscellaneous-form.component.scss"],
 })
-export class ExpenseMiscellaneousFormComponent implements OnInit {
-
-  @ViewChild('expenseForm') form: NgForm;
+export class ExpenseMiscellaneousFormComponent
+  implements OnInit, AfterViewInit
+{
+  @ViewChild("expenseForm") form: NgForm;
   @Input() miscellaneous: Miscellaneous[];
   @Output() changed: EventEmitter<boolean> = new EventEmitter();
   misc: Miscellaneous;
@@ -20,18 +29,24 @@ export class ExpenseMiscellaneousFormComponent implements OnInit {
   miscellaneousTypes = [];
   vatRates = [];
 
-  constructor(public miscellaneousExpensesService: MiscellaneousExpensesService,
-              public timesheetService: TimesheetService,
-              private _monetaryService: MonetaryService) { }
+  constructor(
+    public miscellaneousExpensesService: MiscellaneousExpensesService,
+    public timesheetService: TimesheetService,
+    private _monetaryService: MonetaryService
+  ) {}
 
   ngOnInit() {
     this.misc = new Miscellaneous();
-    this.miscellaneousTypes = this.miscellaneousExpensesService.miscellaneousTypes;
+    this.miscellaneousTypes =
+      this.miscellaneousExpensesService.miscellaneousTypes;
     this.vatRates = [];
-    Object.values(this._monetaryService.vatRates).forEach(rate => {
+    Object.values(this._monetaryService.vatRates).forEach((rate) => {
       this.vatRates.push(rate);
     });
     this.miscellaneous = this.timesheetService.timesheet.miscellaneous;
+  }
+
+  ngAfterViewInit(): void {
     this.form.valueChanges.subscribe(() => {
       if (this.form.dirty) {
         this.changed.emit(true);
@@ -41,21 +56,36 @@ export class ExpenseMiscellaneousFormComponent implements OnInit {
 
   onSubmit() {
     if (this.form.valid) {
-      if (this.miscellaneousExpensesService.vatDeductible(this.misc) && this.misc.selectedType === 1) {
-        this.misc.tvaRate = this.miscellaneousExpensesService.miscellaneousTypes[this.misc.selectedType].vat;
+      if (
+        this.miscellaneousExpensesService.vatDeductible(this.misc) &&
+        this.misc.selectedType === 1
+      ) {
+        this.misc.tvaRate =
+          this.miscellaneousExpensesService.miscellaneousTypes[
+            this.misc.selectedType
+          ].vat;
       }
-      if (!this.miscellaneousExpensesService.vatDeductible(this.misc) && this.misc.selectedType !== 5) {
-        this.misc.tvaRate = this.miscellaneousExpensesService.miscellaneousTypes[this.misc.selectedType].vat;
+      if (
+        !this.miscellaneousExpensesService.vatDeductible(this.misc) &&
+        this.misc.selectedType !== 5
+      ) {
+        this.misc.tvaRate =
+          this.miscellaneousExpensesService.miscellaneousTypes[
+            this.misc.selectedType
+          ].vat;
       }
       if (!this.miscellaneousExpensesService.vatDeductible(this.misc)) {
-        this.misc.tvaRate = 'NC';
+        this.misc.tvaRate = "NC";
       }
-      this.misc.miscellaneousType = this.miscellaneousExpensesService.miscellaneousTypes[this.misc.selectedType].type;
+      this.misc.miscellaneousType =
+        this.miscellaneousExpensesService.miscellaneousTypes[
+          this.misc.selectedType
+        ].type;
       this.submitted = true;
       this.miscellaneous.push(Object.assign(new Miscellaneous(), this.misc));
       this.changed.emit(true);
     } else {
-      Object.keys(this.form.controls).forEach(field => {
+      Object.keys(this.form.controls).forEach((field) => {
         this.form.controls[field].markAsTouched();
       });
     }
