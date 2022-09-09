79042f9e7935149266d68c47351c85e407a83c73
Julien Lavocat <julien.lavocat@gmail.com>
2022-09-09 09:49:23 +0200

    fix: remove dashboard's unused parts

--- src/app/modules/dashboard/dashboard.routes.ts
+++ src/app/modules/dashboard/dashboard.routes.ts
@@ -1,28 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { DashboardComponent } from './dashboard.component';
-import { ActivityComponent } from './views/activity/activity.component';
-import { CompanyComponent } from './views/company/company.component';
-import { MissionAllComponent } from './views/mission/mission-all/mission-all.component';
-import { MissionCreateComponent } from './views/mission/mission-create/mission-create.component';
-import { ProfileComponent } from './views/profile/profile.component';
+import { DashboardComponent } from "./dashboard.component";
+import { ActivityComponent } from "./views/activity/activity.component";
+import { CompanyComponent } from "./views/company/company.component";
+import { MissionAllComponent } from "./views/mission/mission-all/mission-all.component";
+import { MissionCreateComponent } from "./views/mission/mission-create/mission-create.component";
+import { ProfileComponent } from "./views/profile/profile.component";
 
 const routes: Routes = [
-  { path: 'dashboard', redirectTo: 'dashboard/activity' },
+  { path: "dashboard", redirectTo: "dashboard/mission" },
   {
-    path: 'dashboard',
+    path: "dashboard",
     component: DashboardComponent,
     children: [
-      { path: 'profile', component: ProfileComponent },
-      { path: 'company', component: CompanyComponent },
-      { path: 'activity', component: ActivityComponent },
-      { path: 'mission', redirectTo: 'mission/all' },
+      { path: "profile", component: ProfileComponent },
+      { path: "company", component: CompanyComponent },
+      { path: "mission", redirectTo: "mission/all" },
       {
-        path: 'mission',
+        path: "mission",
         children: [
-          { path: 'all', component: MissionAllComponent },
-          { path: 'create', component: MissionCreateComponent },
+          { path: "all", component: MissionAllComponent },
+          { path: "create", component: MissionCreateComponent },
         ],
       },
     ],
