71160250f3bc83b13b3116768d5e1cc0eef3881f
Julien Lavocat <julien.lavocat@gmail.com>
2022-09-13 09:26:38 +0200

    feat(timesheet): use timesheet id instead of encoded data

--- src/app/modules/timesheet/components/timesheet-review/timesheet-review.component.ts
+++ src/app/modules/timesheet/components/timesheet-review/timesheet-review.component.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { Params, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+import { Params, ActivatedRoute } from "@angular/router";
 
-import { Timesheet } from 'src/app/shared/models/timesheet.model';
-import { Commute } from '../../../../shared/models/commute';
+import { Timesheet } from "src/app/shared/models/timesheet.model";
+import { Commute } from "../../../../shared/models/commute";
 
-import { CalendarService } from 'src/app/modules/calendar/calendar.service';
-import { TimesheetService } from '../../services/timesheet.service';
-import { AuthenticationService } from 'src/app/shared/services/authentication/authentication.service';
-import { WorkingEvent } from 'src/app/shared/@types/workingEvent';
+import { CalendarService } from "src/app/modules/calendar/calendar.service";
+import { TimesheetService } from "../../services/timesheet.service";
+import { AuthenticationService } from "src/app/shared/services/authentication/authentication.service";
+import { WorkingEvent } from "src/app/shared/@types/workingEvent";
 
 @Component({
-  selector: 'app-review',
-  templateUrl: './timesheet-review.component.html',
-  styleUrls: ['./timesheet-review.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  selector: "app-review",
+  templateUrl: "./timesheet-review.component.html",
+  styleUrls: ["./timesheet-review.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TimesheetReviewComponent implements OnInit {
   timesheet = new Timesheet();
   generateInvoice = false;
   invoiceLink: string;
   date: Date;
-  locale = 'fr';
+  locale = "fr";
   workedTime: WorkingEvent[];
   commutes: Commute[];
   showAllowanceTable = false;
   showMiscellaneousTable = false;
   transferToken: string;
+  ready: boolean;
 
   constructor(
     public auth: AuthenticationService,
@@ -34,11 +35,11 @@ export class TimesheetReviewComponent implements OnInit {
     private route: ActivatedRoute,
     private timesheetService: TimesheetService,
     private titleService: Title
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      if (this.timesheetService.openTimesheet(params['data'], 'review')) {
+    this.route.params.subscribe(async (params: Params) => {
+      if (await this.timesheetService.openTimesheet(params.data, "review")) {
         this.timesheet = this.timesheetService.timesheet;
         this.date = this.calendarService.getDate(this.timesheet);
         this.workedTime = this.calendarService.getWorkedTime(this.timesheet);
@@ -49,9 +50,33 @@ export class TimesheetReviewComponent implements OnInit {
           this.invoiceLink = this.timesheetService.getInvoiceLink();
           this.generateInvoice = true;
         }
+
+        this.ready = true;
       }
     });
-    this.titleService.setTitle('Acradababra - Consulter un compte rendu d\'activité');
-  }
 
+    // if (this.route.snapshot.params.data) {
+    //   const timesheet = await this.timesheetService.getTimesheet(
+    //     this.route.snapshot.params.data
+    //   );
+
+    //   this.timesheetService.timesheet = timesheet;
+    //   this.timesheet = this.timesheetService.timesheet;
+    //   this.date = this.calendarService.getDate(this.timesheet);
+    //   this.workedTime = this.calendarService.getWorkedTime(this.timesheet);
+    //   this.transferToken = this.timesheetService.getTransferToken();
+    //   this.generateInvoice = false;
+
+    //   if (this.timesheet.invoice) {
+    //     this.invoiceLink = this.timesheetService.getInvoiceLink();
+    //     this.generateInvoice = true;
+    //   }
+    //   this.ready = true;
+    //   console.log(this.timesheetService.timesheet);
+    // }
+
+    this.titleService.setTitle(
+      "Acradababra - Consulter un compte rendu d'activité"
+    );
+  }
 }
--- src/app/shared/models/consultant.model.ts
+++ src/app/shared/models/consultant.model.ts
@@ -1,11 +1,13 @@
+import { Company } from "./company.model";
+
 export class Consultant {
   email: string;
   name: string;
+  company: Company;
 
   constructor(consultantEmail: string, consultantName: string) {
     this.email = consultantEmail;
     this.name = consultantName;
-
+    this.company = new Company();
   }
-
 }
