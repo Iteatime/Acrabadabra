68e18af976231f3fb2a16f876f1166401188b24e
helloyanis <helloyanis@hotmail.com>
2023-01-27 10:44:32 +0100

    feat(company): save button redone
    feat(company) save button redone
    
    Both save buttons merged
    Notification upon saving
    
    fix(company) Change position of save button
    
    fix(company): Disable save button during saving
    
    (chore): Reformat code
    
    fix: update disabled button method
    
    fix(company): update button style + code reformat
    
    Co-Authored-By: Julien Lavocat <JulienLavocat@users.noreply.github.com>

--- src/app/modules/dashboard/views/company/company.component.ts
+++ src/app/modules/dashboard/views/company/company.component.ts
@@ -4,6 +4,7 @@ import { Company } from "../../../../shared/models";
 
 import { State } from "../../@type";
 import { CompanyService, StoreService } from "../../services";
+import { NotificationService } from "src/app/modules/notification/services/notification.service";
 
 @Component({
   selector: "app-company",
@@ -16,8 +17,9 @@ export class CompanyComponent implements OnInit {
 
   constructor(
     private _companyService: CompanyService,
+    private NotificationService: NotificationService,
     public store: StoreService
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.ready = false;
@@ -44,15 +46,33 @@ export class CompanyComponent implements OnInit {
         this.store.setState({ company: response }, (state: State) => {
           if (this.company) this.company = state.company || new Company();
           this.ready = true;
+          this.NotificationService.push("Sauvegarde effectuée!", "success")
         });
       });
   }
 
   onDetailsSubmit(form: NgForm) {
+    form.form.markAsPristine()
     this.updateCompany({
       ...this.company,
-      ...form.value,
-    });
+      name: form.value.name,
+      address: form.value.address,
+      zipCode: form.value.zipCode,
+      city: form.value.city,
+      phone: form.value.phone,
+      siren: form.value.siren,
+      tradeAndCompaniesRegisterCity: form.value.tradeAndCompaniesRegisterCity,
+      vatNumber: form.value.vatNumber,
+      bankAccount: {
+        holder: form.value.holder,
+        swift: form.value.swift,
+        iban: form.value.iban,
+        domiciliation: form.value.domiciliation,
+        agency: form.value.agency,
+      }
+    } as any)
+
+
   }
   onBankSubmit(form: NgForm) {
     this.updateCompany({
