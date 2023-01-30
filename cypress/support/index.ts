30a6cb63477c8a352d51b1b6182e44d3f7be7401
Julien Lavocat <julien.lavocat@gmail.com>
2023-01-30 08:58:46 +0100

    test(cypress): add seeds

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,9 +1,28 @@
 declare namespace Cypress {
   interface Chainable<Subject = any> {
+    /**
+     * Create and initialize a new Cypress session that will be reused for evey tests requiring a login
+     * @param sessionName session to use (defaults to: default-user)
+     */
     netlifyLogin(sessionName?: string): void;
+
+    /**
+     * Returns Netlify's Identity iframe body
+     */
     getNetlifyIdentityFrame(): Chainable<
       JQuery<HTMLIFrameElement | Document | Text | Comment>
     >;
+
+    /**
+     * Check if the current url ends with the specified value
+     * @param value
+     */
     urlEndsWith(value: string): void;
+
+    /**
+     * Seeds MongoDB using the speficied seeds in `seeds/<seedName>`
+     * @param seedName seed to use (defaults to main)
+     */
+    seed(seedName?: string): void;
   }
 }
